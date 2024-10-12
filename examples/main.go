eab4073e18f5ea69ca07d373f10f9a97d9283cee
justinbather <justinbather@hotmail.com>
2024-10-12 11:16:51 +0100

    adds color support

--- examples/main.go
+++ examples/main.go
@@ -5,5 +5,7 @@ import "github.com/justinbather/prettylog"
 func main() {
 	logger := prettylog.New()
 
-	logger.Info("This is a log", "someKey", "someVal")
+	logger.Info("This is an info log", "someKey", "someVal")
+	logger.Warn("This is a warn log", "someKey", "someVal")
+	logger.Error("This is an error log", "someKey", "someVal")
 }
